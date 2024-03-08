8ace92a352b099a3eda5e317fc86876189dd0f13
sibiryak856 <belyakov-semyon@yandex.ru>
2024-03-08 17:03:15 +0300

    push v2

--- src/main/java/ru/practicum/shareit/request/mapper/RequestMapper.java
+++ src/main/java/ru/practicum/shareit/request/mapper/RequestMapper.java
@@ -27,7 +27,7 @@ public interface RequestMapper {
     ItemRequestDto toRequestDto(ItemRequest itemRequest);
 
     default List<ItemRequestDto> toRequestDtosList(List<ItemRequest> itemRequests, Map<Long, List<ItemDto>> requestItems) {
-        if(itemRequests == null) {
+        if (itemRequests == null) {
             return null;
         } else {
             List<ItemRequestDto> list = new ArrayList(itemRequests.size());
@@ -35,7 +35,7 @@ public interface RequestMapper {
 
             while (var3.hasNext()) {
                 ItemRequest itemRequest = (ItemRequest)var3.next();
-                ItemRequestDto requestDto= this.toRequestDto(itemRequest);
+                ItemRequestDto requestDto = this.toRequestDto(itemRequest);
                 requestDto.setItems(requestItems.getOrDefault(requestDto.getId(), Collections.emptyList()));
                 list.add(requestDto);
             }
